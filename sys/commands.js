4c9eec81e699119b310c0c07900fe3a162747575
GhettoGirl <magiruuvelvet@naver.com>
2016-09-20 23:44:52 +0200

    Commands: simplify commands.js for better readability

--- sys/commands.js
+++ sys/commands.js
@@ -5,38 +5,35 @@
  *
  */
 
-const CmdHistory = new (require('../commands/history.js'))(global.settings.commands().history);
-const CmdExit = new (require('../commands/exit.js'))(global.settings.commands().exit);
+const n_cmd = global.settings.commands();
+
+function cmd(name)
+{
+    return "../commands/" + name + ".js";
+}
 
 const commands = [
-    CmdHistory,
-    CmdExit
+    new (require(cmd("history"))) (n_cmd.history),
+    new (require(cmd("exit")))    (n_cmd.exit)
 ];
 
 // check command list for duplicate names
-var has_dups = false;
-var list_size = commands.length;
-for (var i = 0; i < list_size; i++)
+for (var i = 0; i < commands.length; i++)
 {
-    for (var j = 0; j < list_size; j++)
+    for (var j = 0; j < commands.length; j++)
     {
         if (i != j && commands[i].m_name == commands[j].m_name)
         {
-            has_dups = true;
-        }
-    }
-}
-
-// terminate program when command list has duplicates
-// i will not support this behavior and the user should
-// not do this in the first place anyway
-if (has_dups)
-{
-    console.error(global.ansi.style.bold + global.termcolor.foreground.rgb(195, 0, 0)
+            // terminate program when command list has duplicates
+            // i will not support this behavior and the user should
+            // not do this in the first place anyway
+            console.error(global.ansi.style.bold + global.termcolor.foreground.rgb(195, 0, 0)
                 + "FATAL:" + global.ansi.style.reset + " "
                 + "The command list has duplicates, this behavior is not supported!");
-    console.error("Please fix this before you continue using this program.");
-    global.process_cleanup_and_exit(2);
+            console.error("Please fix this before you continue using this program.");
+            global.process_cleanup_and_exit(2);
+        }
+    }
 }
 
 module.exports = commands;
