afec5aa3d5dc4d8626046d98a1514e29db16cbc2
GhettoGirl <magiruuvelvet@naver.com>
2016-09-22 14:18:09 +0200

    Common: improved randomizer
    The randomizer was improved from ground up with lots of
    changes and features.
    
    1. The randomizer now uses MersenneTwister instead of
       JavaScript's own Math.random()
    2. The randomizer now remembers a specific amount of
       previously generated numbers, to prevent the same
       number to be returned too often.
       The history size can be configured freely.
    3. To prevent infinite loops, the randomizer has several
       checks, like if max is greater than min or if
       the history size is larger than the range of numbers
       to generate.

--- utils/randomizer.js
+++ utils/randomizer.js
@@ -2,10 +2,70 @@
  * @randomizer.js
  *
  * Returns a random number within the given range
+ * Remembers the last n numbers, which will never be returned
  *
  */
 
-module.exports = function(min, max)
+// initialize MersenneTwister
+const MersenneTwister = new (require('./mersenne-twister.js'));
+
+// helper function to get ranged results
+function rand_helper(num, min, max)
 {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
+    return Math.floor(num * (max - min + 1) + min);
+}
+
+const method = Randomizer.prototype;
+
+function Randomizer(history_size)
+{
+    this.setHistorySize(history_size);
+    this.m_now = 0;
+
+    // prevent infinite loop, can be caused when the history is larger then
+    // the range between min and max
+    this.m_maxattempts = this.m_history.length + 1;
+}
+
+method.setHistorySize = function(num)
+{
+    this.m_history = [];
+
+    var size = num || 2; // default is 2
+    for (let i = 0; i < size; i++)
+    {
+        this.m_history.push(0);
+    }
+}
+
+method.random = function(min, max)
+{
+    if (min == 0 && max == 0)
+    {
+        return 0;
+    }
+
+    if (min > max)
+    {
+        return 0;
+    }
+
+    this.m_maxattempts = this.m_history.length + 1;
+    while (this.m_history.includes(this.m_now))
+    {
+        if (this.m_maxattempts == 0)
+        {
+            break;
+        }
+
+        this.m_now = rand_helper(MersenneTwister.random(), min, max);
+        this.m_maxattempts--;
+    }
+
+    this.m_history.push(this.m_now);
+    this.m_history.shift();
+
+    return this.m_now;
+}
+
+module.exports = Randomizer;
