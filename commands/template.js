2348366057e3dede799f9a33f44c986493417e2f
GhettoGirl <magiruuvelvet@naver.com>
2016-09-22 23:24:12 +0200

    Commands: add command template

---
+++ commands/template.js
@@ -0,0 +1,25 @@
+/* Music Console
+ * @template.js
+ *
+ * Command: [template]
+ * use this template to create new commands
+ *
+ */
+
+const Command = require('../sys/command.js');
+
+function CmdTemplate(name)
+{
+    Command.call(this, name);
+}
+
+CmdTemplate.prototype = Object.create(Command.prototype);
+CmdTemplate.prototype = {
+
+execute: function(args)
+{
+}
+
+} // prototype
+
+module.exports = CmdTemplate;
