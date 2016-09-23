d31e50269511e95825842858e57cd278fa0b51d3
GhettoGirl <magiruuvelvet@naver.com>
2016-09-23 06:11:59 +0200

    Installer: more functionality, still experimental

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,9 @@
  *
  *  - (Runs tests) - at the moment there aren't any
  *  - Minifies every JavaScript and JSON file
+ *      todo: remove devDependencies from all package.json files
  *  - Skips every unneeded files, except LICENSES
+ *  - Builds native addons
  *
  *  - Installs the release in the './dist' directory
  *
@@ -15,16 +17,13 @@
  *  - write tests
  *
  *  - install required node_modules
- *  - preserve directory structure
- *  - include minified package.json files
  *  - include LICENSE files
- *  - rebuild native addons (node-gyp)
- *  - install native addons
  *  - clean up after install
  *
  *
  */
 
+const path = require('path');
 const gulp = require('gulp');
 const gp_concat = require('gulp-concat');
 const gp_babel = require('gulp-babel');
@@ -32,6 +31,7 @@ const gp_jsonmin = require('gulp-jsonminify');
 const gp_sourcemaps = require('gulp-sourcemaps');
 const combiner = require('stream-combiner2');
 const del = require('del');
+const exec = require('child_process').exec;
 
 const paths = {
     scripts: [
@@ -47,24 +47,38 @@ const paths = {
         "lib/kbhit/*.js",
         "lib/medialibrarymodel/*.js",
         "lib/mediaplayercontroller/*.js",
-        "lib/searchtermgen/*.js"
+        "lib/searchtermgen/*.js",
+
+        "commands/*.js",
+        "!commands/template.js"
+    ],
+    pjsons: [
+        "package.json",
+        "lib/**/package.json",
+        "utils/js-extensions/package.json",
+        "extern/**/package.json"
+    ],
+    native_modules: [
+        "lib/*.node"
     ]
 };
 
+// clean old build
 gulp.task('clean', function()
 {
-    return del(["build", "dist"]);
+    return del(["./build/**", "./dist/**"]);
 });
 
+// minify scripts and install them
 gulp.task('scripts', ['clean'], function()
 {
     var combined = combiner.obj([
-        gulp.src(paths.scripts),
+        gulp.src(paths.scripts, {base: '.'}),
         //.pipe(gp_sourcemaps.init())
         gp_babel({presets: ['babili']}),
         //.pipe(gp_concat('app.js'))
         //.pipe(gp_sourcemaps.write())
-        gulp.dest('./dist')
+        gulp.dest("./dist")
     ]);
 
     combined.on('error', console.error.bind(console));
@@ -72,4 +86,41 @@ gulp.task('scripts', ['clean'], function()
     return combined;
 });
 
-gulp.task('default', ['scripts']);
+// minify package.json's and install them
+gulp.task('pjsons', ['clean'], function()
+{
+    var combined = combiner.obj([
+        gulp.src(paths.pjsons, {base: '.'}),
+        gp_jsonmin(),
+        gulp.dest("./dist")
+    ]);
+
+    combined.on('error', console.error.bind(console));
+
+    return combined;
+});
+
+// (re)-builds native addons
+gulp.task('native-build', ['clean'], function(cb)
+{
+    exec('node-gyp rebuild', function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
+// installs native addons
+gulp.task('native-install', ['clean', 'native-build'], function()
+{
+    var combined = combiner.obj([
+        gulp.src(paths.native_modules, {base: '.'}),
+        gulp.dest("./dist")
+    ]);
+
+    combined.on('error', console.error.bind(console));
+
+    return combined;
+});
+
+gulp.task('default', ['clean', 'scripts', 'pjsons', 'native-build', 'native-install']);
