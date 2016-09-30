af36153d9ea97c94c37181ed1495723eb6ac5c4d
GhettoGirl <magiruuvelvet@naver.com>
2016-09-30 16:31:42 +0200

    extern:version.js: update to latest version

--- version.js
+++ version.js
@@ -9,8 +9,6 @@
  */
 
 const path = require('path');
-const app_dir = path.dirname(require.main.filename);
-const pjson = require(app_dir + "/package.json");
 
 function pkg_version_error(msg)
 {
@@ -19,38 +17,73 @@ function pkg_version_error(msg)
 
 const method = pkg_version.prototype;
 
-function pkg_version()
+function pkg_version(package_json)
 {
-    try
+    var pjson;
+
+    if (typeof package_json == "string")
+    {
+        // use provided package.json file
+        try
+        {
+            pjson = require(path.join(__dirname, package_json));
+        }
+
+        catch (error)
+        {
+            pjson = undefined;
+        }
+    }
+
+    else
+    {
+        // use the application's package.json file
+        try
+        {
+            const app_dir = path.dirname(require.main.filename);
+            pjson = require(app_dir + "/package.json");
+        }
+
+        catch (error)
+        {
+            pjson = undefined;
+        }
+    }
+
+    if (pjson !== undefined)
     {
         var ver = pjson.version.split('.');
 
         if (ver.length != 3)
         {
             pkg_version_error("No valid semantic version string found in package.json");
+            this.m_main = 0;
+            this.m_progress = 0;
+            this.m_revision = 0;
         }
 
         else
         {
-            this.m_main = ver[0];
-
-            if (ver[1].length != 1 && ver[2].length != 1)
+            if (ver[1].length != 1 || ver[2].length != 1 || pjson.revision.indexOf('.') != -1)
             {
                 pkg_version_error("Invalid version string");
+                this.m_main = 0;
+                this.m_progress = 0;
+                this.m_revision = 0;
             }
 
             else
             {
+                this.m_main = ver[0];
                 this.m_progress = ver[1] + ver[2];
+                this.m_revision = pjson.revision;
             }
-
-            this.m_revision = pjson.revision;
         }
     }
 
-    catch (error)
+    else
     {
-        pkg_version_error("Failed to parse package.json");
+        pkg_version_error("No package.json file to parse");
     }
 }
 
