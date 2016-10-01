2951862f4e7b7905654b3394a5398550a54c1285
GhettoGirl <magiruuvelvet@naver.com>
2016-10-01 20:42:20 +0200

    Utils: resolve-env.js now supports Windows too
    Added support for Windows-style (%ENV%) environment variables.

--- utils/resolve-env.js
+++ utils/resolve-env.js
@@ -2,19 +2,31 @@
  * @resolve-env.js
  *
  * Resolves environment variables in strings
- * Only POSIX-style variables are supported
  *
- * TODO: add windows-style environment variable support, %ENVVAR%
+ * Supports both, POSIX and Windows-style environment variables
  *
  */
 
-const re = /\$(([a-zA-Z_]+[a-zA-Z0-9_]*)|\{([a-zA-Z_]+[a-zA-Z0-9_]*)\})/g;
+var re;
+
+if (process.platform == 'win32')
+{
+    // match Windows (%%) style environment variables
+    re = /%([^%]+)%/g;
+}
+
+else
+{
+    // match POSIX ($) style environment variables
+    // fixme: doesn't has the ability to escape the '$' character, otherwise correct
+    re = /\$(([a-zA-Z_]+[a-zA-Z0-9_]*)|\{([a-zA-Z_]+[a-zA-Z0-9_]*)\})/g;
+}
 
 module.exports = function(input)
 {
     return input.replace(re, function($0, $1, $2, $3)
     {
-        var key = $2 || $3;
+        var key = (process.platform == 'win32' ? $1 : $2 || $3);
 
         if (process.env.hasOwnProperty(key))
         {
